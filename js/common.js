0a3f01a8dff3729cca78ffc9e207756767d1f9a6
js <casinodiler@google.com>
2023-01-26 12:03:30 +0900

    mainend

--- js/common.js
+++ js/common.js
@@ -1,3 +1,12 @@
 $(function () {
-    $('.slide_01').slick({})
+    $('.slide_01').slick({
+        autoplay: true,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+    })
+    $('.slide_02').slick({
+        autoplay: true,
+        dots: true,
+        fade: true,
+    })
 })
